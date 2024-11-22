fb7f3800fd365c7411da39db38e2be3a17e0f46b
CristianOrtiz-unicab <cortiz@unicab.org>
2024-11-21 19:15:50 -0500

    homeStyles.css actualizado, añadido inserts, añadido banner.png

---
+++ business/repositories/scriptsHomeUnicab/insert_tbl_imagenes-19-11.sql
@@ -0,0 +1,12 @@
+INSERT INTO tbl_imagenes (id_seccion, ruta, descripcion, titulo, posicionTitulo, rutaEncima, textoAlterno) VALUES
+(1, 'assets/img/banner.png', 'imagenbanner1', '', '', '', '');
+
+
+INSERT INTO tbl_imagenes (id_seccion, ruta, descripcion, titulo, posicionTitulo, rutaEncima, textoAlterno) VALUES
+(2, 'assets/img/boton_bachillerato.png', 'botonBachillerato', '', '', 'assets/img/boton_over_bachillerato.png', '');
+
+
+INSERT INTO tbl_imagenes (id_seccion, ruta, descripcion, titulo, posicionTitulo, rutaEncima, textoAlterno) VALUES
+(2, 'assets/img/boton_ciclos.png', 'botonCiclos', '', '', 'assets/img/boton_over_ciclos.png', '');
+
+
---
+++ business/repositories/scriptsHomeUnicab/insert_tbl_sentencias_19-11.sql
@@ -0,0 +1,8 @@
+INSERT INTO tbl_sentencias (campos, tablas, condiciones, agrupaciones, ordenamientos, modificaciones, condicionesAgrupaciones) VALUES
+('SELECT * ', 'FROM tbl_imagenes ', 'WHERE id_seccion = 1', '', '', '', '');
+
+INSERT INTO tbl_sentencias (campos, tablas, condiciones, agrupaciones, ordenamientos, modificaciones, condicionesAgrupaciones) VALUES
+('SELECT * ', 'FROM tbl_secciones ', 'WHERE id = 2', '', '', '', '');
+
+INSERT INTO tbl_sentencias (campos, tablas, condiciones, agrupaciones, ordenamientos, modificaciones, condicionesAgrupaciones) VALUES
+('SELECT * ', 'FROM tbl_secciones ', 'WHERE id = 1', '', '', '', '');
