1cf052e3097ab1487da7b5ef3bd8e03dacb8f3b0
CristianOrtiz-Unicab <cortiz@unicab.org>
2024-12-19 09:10:52 -0500

    Ajustes en secciones inscripciones y habilidades

--- assets/js/homeJs.js
+++ assets/js/homeJs.js
@@ -2,17 +2,15 @@ $(function () {
     $(".select2").select2();
 });
 
-// Inscripciones abiertas form
 $(document).ready(function () {
     $("#myForm").on("submit", function (e) {
-
         e.preventDefault();
 
-        let nombre = $("#nombre").val();
-        let email = $("#correo").val();
-        let asunto = $("#asunto").val();
-        let mensaje = $("#mensaje").val();
-        let subscribe = $("#checkbox").is(":checked");
+        let nombre = $("#inscripciones_nombre").val();
+        let email = $("#inscripciones_correo").val();
+        let asunto = $("#inscripciones_asunto").val();
+        let mensaje = $("#inscripciones_mensaje").val();
+        let subscribe = $("#inscripciones_checkbox").is(":checked");
 
         const data = {
             nombre: nombre,
@@ -28,15 +26,119 @@ $(document).ready(function () {
             data: data,
             success: function (response) {
                 if (response.status === "success") {
-                    $("#successMessage").fadeIn().delay(3000).fadeOut();
                     $("#myForm")[0].reset();
+                    $("#inscripciones_enviar").attr("disabled", "disabled");
+                    $("#notificacion-success").fadeIn().delay(3000).fadeOut();
+                    reset_clases();
                 } else {
-                    $("#errorMessage").fadeIn().delay(3000).fadeOut();
+                    $("#form-notificacion")
+                        .text("Error al enviar el formulario. Inténtalo de nuevo")
+                        .fadeIn()
+                        .delay(3000)
+                        .fadeOut();
                 }
             },
             error: function (response) {
-                $("#errorMessage").fadeIn().delay(3000).fadeOut();
+                $("#form-notificacion")
+                    .text("Error al enviar el formulario. Inténtalo de nuevo")
+                    .fadeIn()
+                    .delay(3000)
+                    .fadeOut();
             },
         });
     });
 });
+
+const reglasvalidacion  = {
+    texto: /[-_'"\<\>\~\^\*\$\!\¡\#\%\&\¿\?\/\=\+\|,;:\(\)\{\}\[\]\\]{1,}/,
+    texto1: /[_'"\<\>\~\^\*\$\!\¡\#\%\&\¿\?\/\=\+\|,;:\(\)\{\}\[\]\\]{1,}/,
+    nombre: /^[a-zA-ñÑ\s]{1,100}$/,
+    correo: /^[_-\w.]+@[a-z]+\.[a-z]{2,5}$/,
+    asunto: /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\s]{1,200}$/,
+    mensaje: /^[\wáéíóúÁÉÍÓÚñÑ.,!?¿¡()\-:\s]{1,2000}$/,
+};
+
+// Elemento donde se muestra el error
+let notificacionFormInscripciones;
+
+// Función para validar un campo
+function validar_campo(input) {
+    if (!notificacionFormInscripciones) {
+        notificacionFormInscripciones =
+            document.querySelector("#form-notificacion");
+    }
+
+    let { name, value, type } = input;
+
+    if (type === "checkbox") {
+        if (input.checked) {
+            input.classList.remove("error");
+            input.classList.add("success");
+        } else {
+            input.classList.add("error");
+            input.classList.remove("success");
+        }
+    } else if (reglasvalidacion[name].test(value)) {
+        notificacionFormInscripciones.innerText = ""; // Limpia errores
+        notificacionFormInscripciones.classList.add("notificacion-hidden");
+        input.classList.remove("error");
+        input.classList.add("success");
+    } else {
+        notificacionFormInscripciones.classList.remove("notificacion-hidden");
+        notificacionFormInscripciones.innerText = mensaje_personalizado(name, value);
+        input.classList.add("error");
+        input.classList.remove("success");
+    }
+    
+    mostrar_submit();
+}
+
+const mensaje_personalizado = (campo, value) => {
+    if (campo === "nombre") {
+        if (value.length > 100) {
+            return "El nombre debe contener solo letras y espacios, y no exceder 100 caracteres.";
+        }
+    } else if (campo === "correo") {
+        if (value.length > 50) {
+            return "El correo electrónico debe ser válido y tener un máximo de 50 caracteres (ejemplo: nombre@dominio.com).";
+        }
+    } else if (campo === "asunto") {
+        if (value.length > 200) {
+            return "El asunto debe contener solo letras, números y puntuación válida, con un máximo de 200 caracteres.";
+        }
+    } else if (campo === "mensaje") {
+        if (value.length > 2000) {
+            return "El mensaje puede contener letras, números y símbolos permitidos, con un máximo de 2000 caracteres.";
+        }
+    }
+    return ("Ha ingresado alguno de los siguientes caracteres no válidos para " + campo + ": " + "- _ ' \" < > ~ ^ * $ ! ¡ # % & ¿ ? /= + , ; : ( ) { } [ ] \\");
+};
+
+const mayus = (element) => {
+    element.value = element.value.toUpperCase();
+    validar_campo(element);
+};
+
+const mostrar_submit = () => {
+    const elementosForm = document.querySelectorAll("input, textarea, select");
+
+    let todosValidos = true;
+    elementosForm.forEach((input) => {
+        if ((input.type != "submit" && !input.classList.contains("success")) || input.classList.contains("error")) {
+            todosValidos = false;
+        }
+    });
+
+    if (todosValidos) {
+        $("#inscripciones_enviar").removeAttr("disabled");
+    } else {
+        $("#inscripciones_enviar").attr("disabled", "disabled");
+    }
+};
+
+const reset_clases = () => {
+    const elementosForm = document.querySelectorAll("input, textarea, select");
+    elementosForm.forEach((elementoForm) =>
+        elementoForm.classList.remove("success", "error")
+    );
+};
--- components/descubreHabilidades.php
+++ components/descubreHabilidades.php
@@ -1,84 +1,85 @@
 <?php
 
-function posicionTituloImagen($imgHTML, $titulo, $posicionTitulo)
-{
-    if (strtolower($posicionTitulo) == 'abajo') {
-        return
-            '<div class="img-container col-6 col-sm-4 col-lg-2 d-flex flex-column align-items-center gap-4 p-0 my-3">' . "\n"
-            . $imgHTML . "\n"
-            . '<p>' . $titulo . '</p>' . "\n"
-            . '</div>' . "\n";
-    } else if (strtolower($posicionTitulo) == 'derecha') {
-        return
-            '<div class="img-container col-6 col-sm-4 col-lg-2 d-flex align-items-center gap-4 p-0 mx-3">' . "\n"
-            . $imgHTML . "\n"
-            . '<p>' . $titulo . '</p>' . "\n"
-            . '</div>' . "\n";
-    } else if (strtolower($posicionTitulo) == 'izquierda') {
-        return
-            '<div class="img-container col-6 col-sm-4 col-lg-2 d-flex flex-row-reverse align-items-center gap-4 p-0 mx-3">' . "\n"
-            . $imgHTML . "\n"
-            . '<p>' . $titulo . '</p>' . "\n"
-            . '</div>' . "\n";
-    } else if (strtolower($posicionTitulo) == 'arriba') {
-        return
-            '<div class="img-container col-6 col-sm-4 col-lg-2 d-flex flex-column-reverse align-items-center gap-2 p-0 my-3">' . "\n"
-            . $imgHTML . "\n"
-            . '<p>' . $titulo . '</p>' . "\n"
-            . '</div>' . "\n";
+    function posicionTituloImagen($imgHTML, $titulo, $posicionTitulo)
+    {
+        if (strtolower($posicionTitulo) == 'abajo') {
+            return
+                '<a href="#/" class="img-container col-md-2 d-flex flex-column align-items-center gap-4 p-0 my-3">' . "\n"
+                    . $imgHTML . "\n"
+                    . '<p>' . $titulo . '</p>' . "\n"
+                . '</a>' . "\n";
+        } else if (strtolower($posicionTitulo) == 'derecha') {
+            return
+                '<a href="#/" class="img-container col-md-2 d-flex align-items-center gap-4 p-0 mx-3">' . "\n"
+                    . $imgHTML . "\n"
+                    . '<p>' . $titulo . '</p>' . "\n"
+                . '</a>' . "\n";
+        } else if (strtolower($posicionTitulo) == 'izquierda') {
+            return
+                '<a href="#/" class="img-container col-md-2 d-flex flex-row-reverse align-items-center gap-4 p-0 mx-3">' . "\n"
+                    . $imgHTML . "\n"
+                    . '<p>' . $titulo . '</p>' . "\n"
+                . '</a>' . "\n";
+        } else if (strtolower($posicionTitulo) == 'arriba') {
+            return
+                '<a href="#/" class="img-container col-md-2 d-flex flex-column-reverse align-items-center gap-2 p-0 my-3">' . "\n"
+                . $imgHTML . "\n"
+                . '<p>' . $titulo . '</p>' . "\n"
+                . '</a>' . "\n";
+        }
+        return '';
     }
-    return '';
-}
 
-if ($nivel == "raiz") {
-    require('business/repositories/1cc2s4Home.php');
-} else if ($nivel == "uno") {
-    require('../business/repositories/1cc2s4Home.php');
-} else if ($nivel == "dos") {
-    require('../../business/repositories/1cc2s4Home.php');
-} else if ($nivel == "tres") {
-    require('../../../business/repositories/1cc2s4Home.php');
-}
+    if ($nivel == "raiz") {
+        require('business/repositories/1cc2s4Home.php');
+    } else if ($nivel == "uno") {
+        require('../business/repositories/1cc2s4Home.php');
+    } else if ($nivel == "dos") {
+        require('../../business/repositories/1cc2s4Home.php');
+    } else if ($nivel == "tres") {
+        require('../../../business/repositories/1cc2s4Home.php');
+    }
 
-$res_sentecia = $mysqli1->query($sentencia . "5");
-while ($row_sentencia = $res_sentecia->fetch_assoc()) {
-    $sql_seccion_habilidades = $row_sentencia['campos'] . $row_sentencia['tablas'] . $row_sentencia['condiciones'];
-}
-$res_seccion_habilidades = $mysqli1->query($sql_seccion_habilidades);
-
-$html = '';
-
-while ($row_datos_seccion = $res_seccion_habilidades->fetch_assoc()) {
-    // Obtiene el titulo de la sección y lo renderiza
-    $html .= '<section class="container">' . '<div class="row mt-10 mx-0">' .
-        '<h2 class="col text-center titulo-1 px-0">' . "\n" . $row_datos_seccion['titulo'] . '</h2>' . "\n";
-
-    //Obtener subtitulo
-    $subtitulo = explode(" ", $row_datos_seccion['subTitulo']);
-    $html .= '<h3 class="text-center titulo-2">' . '<span class="fw-bold">' . $subtitulo[0] . '</span> ' . $subtitulo[1] . '</h3>' . "\n";
-    //texto de esta sección 
-    $html .= '<p class="text-center texto-base">' . $row_datos_seccion['texto'] . '</p>' . "\n" .
-        '</div>' . "\n";
-}
-
-if ($html !== '') {
-
-    $res_sentecia = $mysqli1->query($sentencia . "18");
-
+    $res_sentecia = $mysqli1->query($sentencia . "5");
     while ($row_sentencia = $res_sentecia->fetch_assoc()) {
-        $sql_imagenes = $row_sentencia['campos'] . $row_sentencia['tablas'] . $row_sentencia['condiciones'];
+        $sql_seccion_habilidades = $row_sentencia['campos'] . $row_sentencia['tablas'] . $row_sentencia['condiciones'];
+    }
+    $res_seccion_habilidades = $mysqli1->query($sql_seccion_habilidades);
+
+    $html = '';
+
+    while ($row_datos_seccion = $res_seccion_habilidades->fetch_assoc()) {
+        // Obtiene el titulo de la sección y lo renderiza
+        $html .= 
+        '<section class="container">' . '<div class="row mt-10 mx-0">' .
+            '<h2 class="col text-center titulo-1 px-0">' . "\n" . $row_datos_seccion['titulo'] . '</h2>' . "\n";
+
+        //Obtener subtitulo
+        $subtitulo = explode(" ", $row_datos_seccion['subTitulo']);
+        $html .= '<h3 class="text-center titulo-2">' . '<span class="fw-bold">' . $subtitulo[0] . '</span> ' . $subtitulo[1] . '</h3>' . "\n";
+        //texto de esta sección 
+        $html .= '<p class="text-center texto-base">' . $row_datos_seccion['texto'] . '</p>' . "\n" .
+            '</div>' . "\n";
     }
 
-    $res_imagenes = $mysqli1->query($sql_imagenes);
+    if ($html !== '') {
 
-    $html .= '<div class="row px-5 mt-10 contenedor-imagenes">' . "\n";
+        $res_sentecia = $mysqli1->query($sentencia . "18");
 
-    while ($row_imagenes = $res_imagenes->fetch_assoc()) {
-        $attributes = ImageAttributeBuilder::buildAttributes($nivel, $row_imagenes['ruta'], $row_imagenes['descripcion'], $row_imagenes['rutaEncima']);
-        $html .= posicionTituloImagen('<img class="habilidades-img" ' . $attributes . '>', $row_imagenes['titulo'], $row_imagenes['posicionTitulo']);
+        while ($row_sentencia = $res_sentecia->fetch_assoc()) {
+            $sql_imagenes = $row_sentencia['campos'] . $row_sentencia['tablas'] . $row_sentencia['condiciones'];
+        }
+
+        $res_imagenes = $mysqli1->query($sql_imagenes);
+
+        $html .= '<div class="row px-5 mt-10 contenedor-imagenes">' . "\n";
+
+        while ($row_imagenes = $res_imagenes->fetch_assoc()) {
+            $attributes = ImageAttributeBuilder::buildAttributes($nivel, $row_imagenes['ruta'], $row_imagenes['descripcion'], $row_imagenes['rutaEncima']);
+            $html .= posicionTituloImagen('<img class="habilidades-img" ' . $attributes . '>', $row_imagenes['titulo'], $row_imagenes['posicionTitulo']);
+        }
+
+        $html .= '</div>' . "\n"
+            . '</section>' . "\n";
     }
-
-    $html .= '</div>' . "\n"
-        . '</section>' . "\n";
-}
-echo $html;
+    echo $html;
