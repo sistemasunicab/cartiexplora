b8258e0552c8784b8b66f654d47e56aac96a8b9f
sistemasunicab <sistemasunicab@gmail.com>
2025-03-05 15:29:49 -0500

    ajuste directorio

--- business/org/components/institutionalDirectory/directory.php
+++ business/org/components/institutionalDirectory/directory.php
@@ -1,5 +1,5 @@
 <?php
-$numero_de_sentencia_nosotros = "36";
+$numero_de_sentencia_nosotros = "45";//36
 $res_sentencia_nosotros = $mysqli1->query($sentencia . $numero_de_sentencia_nosotros);
 while ($row_sentencia_nosotros = $res_sentencia_nosotros->fetch_assoc()) {
     $condiciones_nosotros = str_replace('|', '\'', $row_sentencia_nosotros['condiciones']);
@@ -12,7 +12,7 @@ if (mysqli_num_rows($res_datos_nosotros) > 0) {
     $html_nosotrosImgUno = '<div class="nosotrosImagenesUno">';
     $html_nosotrosImgUno .= '<div class="container-nosotrosImgUno">';
 
-    $numero_de_sentencia_nosotros = "31";
+    $numero_de_sentencia_nosotros = "40";//31
     $res_sentencia_nosotros = $mysqli1->query($sentencia . $numero_de_sentencia_nosotros);
     while ($row_sentencia_nosotros = $res_sentencia_nosotros->fetch_assoc()) {
         $condiciones_nosotros = str_replace('|', '\'', $row_sentencia_nosotros['condiciones']);
