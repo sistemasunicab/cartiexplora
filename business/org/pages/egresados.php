3471b456a76dd3e5c157dc3912bb33eed00dcc7c
Miguelunicab <miguelangelortiz@unicab.org>
2025-06-05 15:11:42 -0500

    Añadiendo libreria PHPMailer, terminando pagina Egresados.

--- business/org/pages/egresados.php
+++ business/org/pages/egresados.php
@@ -7,7 +7,7 @@
 <html lang="en">
 <?php 
     $nivel = "tres";
-    $page_title = "Egresados UNICAB";
+    $page_title = "Promocion 2024 UNICAB";
     include('../../../components/headMain.php');
 ?>
 <body>
@@ -15,6 +15,11 @@
         $nivel = "tres";
         include('../../../components/navBar.php');
         
+        include('../components/egresados/egresadosOnce.php');
+        include('../components/egresados/egresadosCiclos.php');
+        include('../components/egresados/egresadosNoveno.php');
+        include('../components/egresados/egresadosQuinto.php');
+
         include('../../../components/footer.php');
         include('../../../components/bookstoresMain.php');
     ?>  
